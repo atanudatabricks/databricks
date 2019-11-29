c15c92850b9517d49b583ee7c637f82fa429817d
atanudatabricks <58334997+atanudatabricks@users.noreply.github.com>
2019-11-29 19:22:01 +0530

    tt

---
+++ notebooks/Users/atanu.sarkar@databricks.com/Init Atanu.sql
@@ -0,0 +1,18 @@
+-- Databricks notebook source
+-- MAGIC %python
+-- MAGIC 1+1
+
+-- COMMAND ----------
+
+-- MAGIC %sh ls /dbfs/databricks/init/global-init.sh
+
+-- COMMAND ----------
+
+-- MAGIC %sh cat /dbfs/databricks/init/global-init.sh
+
+-- COMMAND ----------
+
+-- MAGIC %sh rm /dbfs/databricks/init/global-init.sh
+
+-- COMMAND ----------
+
